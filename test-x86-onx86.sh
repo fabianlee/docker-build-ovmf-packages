0856a4252c8d4ef01ebee2226a5ac8670f23dd77
Fabian Lee <flee999@hotmail.com>
2023-12-18 09:17:51 -0500

    test on x86

--- test-x86-onx86.sh
+++ test-x86-onx86.sh
@@ -1,1 +1,7 @@
-qemu-system-x86_64 -boot menu=on,splash-time=15000 -bios Build/OvmfX64/RELEASE_GCC5/FV/OVMF.fd --enable-kvm
+qemu-system-x86_64 \
+  -boot menu=on,splash-time=3000 \
+  -cpu host \
+  --accel kvm \
+  -display default,show-cursor=on \
+  -net none \
+  -bios Build/OvmfX64/RELEASE_GCC5/FV/OVMF.fd
